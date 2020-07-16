84c0a48fa52c9e6fa148f81978b34173643066ea
manhvu <manhvu@orgos.net>
2020-07-16 17:07:55 +0700

    rename docker file

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker build -t manhvu2507/dynamodb-admin-docker -f dynamodb-local-docker .
+docker build -t manhvu2507/dynamodb-admin-docker -f dynamodb-admin-docker .
 
 # docker push manhvu2507/dynamodb-admin-docker
